303b7ebebbc0d3f8482814c2eff5f38e2384bf01
Jozef Beneš <benesjo6@fit.cvut.cz>
2016-11-01 09:22:43 +0100

    first commit

---
+++ src/adp/hw01/StrategyMain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adp.hw01;
+
+/**
+ *
+ * @author benesjo6
+ */
+public class StrategyMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
